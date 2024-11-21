17d669b31cecc22ecfa156c8704d0d06d3a4a258
memoTux <romeo@mendezfuentes.net>
2024-11-21 10:30:37 -0600

    docs(components): PlotRender use lifecycle hooks and template ref

--- docs/components/PlotRender.js
+++ docs/components/PlotRender.js
@@ -1,97 +1,110 @@
 import * as Plot from "@observablehq/plot";
-import {h, withDirectives} from "vue";
+import {h, defineComponent} from "vue";
 import {Document} from "./document";
 import {toHyperScript} from "./toHyperScript";
 
-export default {
+export default defineComponent({
+  name: "PlotRender",
   props: {
     options: Object,
     mark: Object,
     defer: Boolean,
     method: {type: String, default: "plot"}
   },
+  computed: {
+    opts() {
+      return {
+        ...(this.method === "plot" && {
+          marks: !this.mark ? [] : [this.mark],
+          width: 688 // better default for VitePress
+        }),
+        ...this.options,
+        className: "plot"
+      };
+    }
+  },
+  created() {
+    this._observer = null;
+    this._idling = null;
+  },
+  mounted() {
+    if (this.defer) {
+      this.observe();
+    }
+  },
+  updated() {
+    if (this.defer) {
+      this.observe();
+    } else {
+      this.replace();
+    }
+  },
+  unmounted() {
+    if (this.defer) {
+      this.clean();
+    }
+  },
+  methods: {
+    observe() {
+      this.disconnect(); // remove old listeners
+      const rect = this.$refs.el.getBoundingClientRect();
+      if (rect.bottom > 0 && rect.top < window.innerHeight) {
+        this.replace();
+      } else {
+        this._observer = new IntersectionObserver(
+          ([entry]) => {
+            if (entry.isIntersecting) this.replace();
+          },
+          {rootMargin: "100px"}
+        );
+        this._observer.observe(this.$refs.el);
+        if (typeof requestIdleCallback === "function") {
+          this._idling = requestIdleCallback(this.replace);
+        }
+      }
+    },
+    disconnect() {
+      if (this._observer !== null) {
+        this._observer.disconnect();
+        this._observer = null;
+      }
+      if (this._idling !== null) {
+        cancelIdleCallback(this._idling);
+        this._idling = null;
+      }
+    },
+    clean() {
+      while (this.$refs.el.lastChild) this.$refs.el.lastChild.remove();
+      this.disconnect();
+    },
+    replace() {
+      this.clean();
+      this.$refs.el.append(Plot[this.method](this.opts));
+    }
+  },
   render() {
     const {method} = this;
-    const options = {
-      ...(method === "plot" && {
-        marks: this.mark == null ? [] : [this.mark],
-        width: 688 // better default for VitePress
-      }),
-      ...this.options,
-      className: "plot"
-    };
     if (this.defer) {
-      const mounted = (el) => {
-        disconnect(); // remove old listeners
-        function observed() {
-          unmounted(el); // remove old plot (and listeners)
-          el.append(Plot[method](options));
-        }
-        const rect = el.getBoundingClientRect();
-        if (rect.bottom > 0 && rect.top < window.innerHeight) {
-          observed();
-        } else {
-          this._observer = new IntersectionObserver(
-            ([entry]) => {
-              if (entry.isIntersecting) observed();
-            },
-            {rootMargin: "100px"}
-          );
-          this._observer.observe(el);
-          if (typeof requestIdleCallback === "function") {
-            this._idling = requestIdleCallback(observed);
-          }
-        }
-      };
-      const unmounted = (el) => {
-        while (el.lastChild) el.lastChild.remove();
-        disconnect();
-      };
-      const disconnect = () => {
-        if (this._observer !== undefined) {
-          this._observer.disconnect();
-          this._observer = undefined;
-        }
-        if (this._idling !== undefined) {
-          cancelIdleCallback(this._idling);
-          this._idling = undefined;
-        }
-      };
-      const {height = 400} = this.options;
-      return withDirectives(
-        h(
-          "span",
-          method === "plot"
-            ? [
-                h("div", {
-                  style: {
-                    maxWidth: "100%",
-                    width: `688px`,
-                    aspectRatio: `688 / ${height}`
-                  }
-                })
-              ]
-            : []
-        ),
-        [
-          [
-            {
-              mounted,
-              updated: mounted,
-              unmounted
-            }
-          ]
-        ]
+      const height = this.options?.height || 400;
+      return h(
+        "span",
+        method === "plot"
+          ? [
+              h("div", {
+                style: {
+                  maxWidth: "100%",
+                  width: `688px`,
+                  aspectRatio: `688 / ${height}`
+                },
+                ref: "el"
+              })
+            ]
+          : []
       );
     }
     if (typeof document !== "undefined") {
-      const plot = Plot[method](options);
-      const replace = (el) => {
-        while (el.lastChild) el.lastChild.remove();
-        el.append(plot);
-      };
-      return withDirectives(h("span", [toHyperScript(plot)]), [[{mounted: replace, updated: replace}]]);
+      return h("span", {ref: "el"}, [toHyperScript(Plot[method](this.opts))]);
     }
-    return h("span", [Plot[method]({...options, document: new Document()}).toHyperScript()]);
+    return h("span", [Plot[method]({...this.opts, document: new Document()}).toHyperScript()]);
   }
-};
+});
